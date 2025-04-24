0866a4b7cb902bf88793698eb7f1655e665d10e8
Rightguy <rightguydaniel@gmail.com>
2025-04-24 09:41:47 +0100

    fix

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,7 +1,9 @@
 import express from "express"
+import { registerUser } from "../controllers/usersControllers/registerUser"
+import { loginUser } from "../controllers/usersControllers/loginUser"
 
 const usersRoutes = express.Router()
-usersRoutes.post("/register")
-usersRoutes.post("/login")
+usersRoutes.post("/register", registerUser)
+usersRoutes.post("/login", loginUser)
 
 export default usersRoutes
