6f525b03a6bc1d921dceaf934be613e8cf4c713e
Rightguy <rightguydaniel@gmail.com>
2025-04-24 16:54:14 +0100

    fix

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import aiRoutes from "./aiRoutes";
 import usersRoutes from "./usersRoutes";
+import { index } from "../controllers";
 
 const indexRoutes = express.Router();
+indexRoutes.get("/", index)
 indexRoutes.use("/ai", aiRoutes)
 indexRoutes.use("/users", usersRoutes)
 
