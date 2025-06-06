db2959e56d55f9e84383c4316b05c5dd18c9bd25
Rightguy <rightguydaniel@gmail.com>
2025-06-06 09:09:37 +0100

    fixed route issue

--- src/routes/aiRoutes.ts
+++ src/routes/aiRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import { customerPreference } from "../controllers/aiControllers/customerPreference.ts";
-import { findPeople } from "../controllers/aiControllers/findPeople.js";
+// import { findPeople } from "../controllers/aiControllers/findPeople.js";
 
 const aiRoutes = express.Router();
 aiRoutes.post("/customer-pref", customerPreference);
-aiRoutes.get("/find", findPeople);
+// aiRoutes.get("/find", findPeople);
 
 export default aiRoutes;
