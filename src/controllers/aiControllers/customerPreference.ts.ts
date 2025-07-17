feab6e3d54141fbc0fdbd15099b257772616654c
Rightguy <rightguydaniel@gmail.com>
2025-07-17 16:57:33 +0100

    fix

--- src/controllers/aiControllers/customerPreference.ts.ts
+++ src/controllers/aiControllers/customerPreference.ts.ts
@@ -151,8 +151,9 @@ export async function getCRMInsights(
       Add dollar symbol and commas in the figure where necessary. Example: $300,000
       * company_size: strings separated by commas
       The number range of employees working for the company. This enables you to find companies based on headcount. You can add multiple ranges to expand your search results.
-      Each range you add needs to be a string, with the upper and lower numbers of the range separated only by a comma.
-      Examples: 1-10; 250-500; 10,000-20,000
+      Each range you add needs to be a string, with the upper and lower numbers of the range separated only by -.
+      Examples: 1-10, 250-500, 10,000-20,000
+      always return 3 ranges for company_size:
       for ***High Net Worth Individual*** company_size should always be very large for example 500,1000,10000,20000,50000,100000,200000,500000
       * role: strings separated by commas
       Job titles held by the people you want to find. For a person to be included in search results, they only need to match 1 of the job titles you add. Adding more job titles expands your search results.
