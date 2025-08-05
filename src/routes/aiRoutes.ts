b5b5deb0e77abb9611d383bbcaa5517628bda69f
Rightguy <rightguydaniel@gmail.com>
2025-08-05 18:04:54 +0100

    fix

--- src/routes/aiRoutes.ts
+++ src/routes/aiRoutes.ts
@@ -1,13 +1,11 @@
 import express from "express";
 import { customerPreference } from "../controllers/aiControllers/customerPreference.ts";
-import { refreshLeads } from "../utils/services/refreshLeads.js";
 import { leadsPrompt } from "../controllers/aiControllers/leadPrompt.js";
 import { userAuth } from "../middlewares/userAuth.js";
 // import { findPeople } from "../controllers/aiControllers/findPeople.js";
 
 const aiRoutes = express.Router();
 aiRoutes.post("/customer-pref", customerPreference);
-aiRoutes.get("/find", refreshLeads);
 aiRoutes.post("/leads-prompt", userAuth, leadsPrompt);
 
 export default aiRoutes;
