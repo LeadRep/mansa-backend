99b28c68366450989b94ad5f08ca06afc9bbedbc
Rightguy <rightguydaniel@gmail.com>
2025-08-19 11:51:57 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { database } from "./configs/database/database";
 import { json, urlencoded } from "body-parser";
 import logger from "morgan";
 import cron from "node-cron";
+import { pinoHttpMiddleware, httpLoggingMiddleware } from "./middlewares/httpLoggingMiddleware";
 import path from "path";
 // import * as glob from "glob";
 // import serveFavicon from "serve-favicon";
@@ -19,8 +20,13 @@ const app = express();
 const port = process.env.APP_PORT || 3000;
 app.use(json());
 app.use(urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: process.env.APP_DOMAIN,
+    credentials: true
+}));
 app.use(logger("dev"));
+// Pino for incoming HTTP logging- duplicated with morgan. one will be removed later
+app.use(pinoHttpMiddleware);
 app.use(cookieParser());
 app.get("/", (request: Request, response: Response) => {
   response.redirect("/v1");
@@ -55,5 +61,7 @@ cron.schedule(
 app.listen(port, () => {
   console.log(`App running at port ${port}`);
 });
+
+app.use(httpLoggingMiddleware);
 
 export default app;
