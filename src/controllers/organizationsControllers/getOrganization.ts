05cc0f05f0b9419cc74290f11564dac6f24c3cd4
Eblohoue Herve <herve.eblohoue@gmail.com>
2025-10-05 05:52:59 +0100

    improve getOrganization

--- src/controllers/organizationsControllers/getOrganization.ts
+++ src/controllers/organizationsControllers/getOrganization.ts
@@ -2,25 +2,47 @@ import { Request, Response } from "express";
 import Organizations from "../../models/Organizations";
 import sendResponse from "../../utils/http/sendResponse";
 import logger from "../../logger";
+import Teams from "../../models/Teams";
+import Users from "../../models/Users";
 
 export const getOrganization = async (request: Request, response: Response) => {
     try {
+        const userId = request.user?.id;
+
         const { organization_id } = request.params;
 
         if (!organization_id) {
             return sendResponse(response, 400, "Organization ID is required");
         }
 
-        const organization = await Organizations.findOne({
+        const org = await Organizations.findOne({
             where: { organization_id },
         });
 
-        if (!organization) {
+        if (!org) {
             return sendResponse(response, 404, "Organization not found");
         }
 
+        const users = await Users.findAll(
+            { where: { organization_id: organization_id } }
+        );
+        const userResp = users.map(u =>  u.get({plain: true}));
+
+        //check if userId is part of userResp
+        const isCallerPartOfOrg = userResp.some(u => u.id === userId);
+
+        if (!isCallerPartOfOrg) {
+            sendResponse(response, 403, "Forbidden: insufficient permissions");
+        }
+
+        const teams = await Teams.findAll(
+            { where: { organization_id: organization_id } }
+        );
+        const teamsResp = teams.map(team => team.get({plain: true}))
+        const organization = { ...org.get({plain: true}), teams: teamsResp, users: userResp };
+
         return sendResponse(response, 200, "Organization retrieved successfully", {
-            organization,
+            organization
         });
     } catch (error: any) {
         logger.error(error, "Get Organization Error:");
