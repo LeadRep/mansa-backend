333c5e56c5c92ee77c952498d8412835af89ab03
Eblohoue Herve <herve.eblohoue@gmail.com>
2026-01-18 08:49:00 +0000

    new http client with improved logs

---
+++ src/utils/http/services/apolloService.ts
@@ -0,0 +1,26 @@
+// src/utils/services/apolloService.ts
+import { apiClient, ApiResponse } from '../apiClient';
+import {RequestConfig} from "../RequestConfig";
+
+export class ApolloService {
+  private baseUrl = 'https://api.apollo.io/v1';
+  private apiKey = process.env.APOLLO_API_KEY!;
+
+  async request<T = any>(endpoint: string, data?: any, config: RequestConfig = {}): Promise<ApiResponse<T>> {
+    return apiClient.post<T>(
+      `${this.baseUrl}/${endpoint}`,
+      data,
+      {
+      ...config,
+      headers: {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+        accept: "application/json",
+        'x-api-key': this.apiKey,
+        ...config.headers,
+      },
+    });
+  }
+}
+
+export const apolloService = new ApolloService();
