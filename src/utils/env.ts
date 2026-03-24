620d06231a2e1087f0b134bac0aa401a6e591927
APPLE <APPLE@MacBookPro.mshome.net>
2026-03-24 06:53:52 -0700

    fix

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+
+dotenv.config();
 export const isProdEnv = (): boolean => {
   return process.env.APP_ENV === "poduction" || process.env.APP_ENV === "production";
 };
